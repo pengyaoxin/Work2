dfde274c6640b9022697d85d973866402b6cf511
pengyaoxin <826550396@qq.com>
2016-09-22 11:16:08 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150738/work2/MainActivity.java
@@ -0,0 +1,52 @@
+
+package cn.edu.gdmec.s07150738.work2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart() called with: " + "我要开始了");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume() called with: " + "我要显示了");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause() called with: " + "我失去焦点了");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop() called with: " + "有人挡在我前面了");
+        super.onStop();
+    }
+
+   /* @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart() called with: " + "");
+        super.onRestart();
+    }
+*/
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy() called with: " + "我要挂了");
+        super.onDestroy();
+    }
+
+}
